a1f3092bbf437aed41c40e79a1bb2ed720674af4
samir <samirbabayevdir@gmail.com>
2021-05-04 19:36:34 +0400

    base sceletion of mvc

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-include 'controller/homeController.php';
+$section = $_GET['section'] ?? 'home';
+
+if ($section == 'about-us') {
+  include_once 'controller/aboutUsController.php';
+} else if ($section == 'contact-us') {
+  include_once 'controller/contactUsController.php';
+} else {
+  include_once 'controller/homeController.php';
+}
